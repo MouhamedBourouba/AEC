8dd9fbae41ae979e7fdece3272f8f72837bb0894
MouhamedBourouba <110403890+MouhamedBourouba@users.noreply.github.com>
2026-04-17 09:41:13 +0100

    basic web server

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
-# Flask app factory
+from flask import Flask
+
+def create_app():
+    # Initialize the app and point it to the templates and static folders in the project root
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+    # Register blueprints
+    from .routes.views import views_bp
+    app.register_blueprint(views_bp)
+
+    return app
--- app/routes/views.py
+++ app/routes/views.py
@@ -1,1 +1,7 @@
-# HTML page routes
+from flask import Blueprint, render_template
+
+views_bp = Blueprint('views', __name__)
+
+@views_bp.route('/')
+def index():
+    return render_template('index.html')
